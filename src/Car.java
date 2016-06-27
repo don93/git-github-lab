4b7054e7c4397e8c8ad9f3b393721d7e595ff1df
Matthew Duffin <matthewtduffin@Matthews-MacBook-Pro.local>
2016-06-27 15:31:59 -0700

    Java files added, main method created

---
+++ src/Car.java
@@ -0,0 +1,5 @@
+/**
+ * Created by matthewtduffin on 27/06/2016.
+ */
+public class Car {
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by matthewtduffin on 27/06/2016.
+ */
+public class Main {
+    public static void main (String[] args) {
+        
+    }
+}
